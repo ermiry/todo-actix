30a9f28ab61cfe748ed769fef05ce85e695e18d6
ermiry <erick.salas@ermiry.com>
2021-02-01 19:15:20 -0600

    Added base actix HTTP server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{ HttpServer, App, Responder, web };
+
+async fn status() -> impl Responder {
+    "{ \"status\": \"ok!\" }"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result <()> {
+    println! ("Starting server at http://127.0.0.1:8080");
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(status))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
