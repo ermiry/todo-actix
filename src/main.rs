7b6ace390231e724d851aa8524d334e85dd25e95
ermiry <erick.salas@ermiry.com>
2021-02-03 13:58:20 -0600

    Getting base server config from env

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use dotenv::dotenv;
 use actix_web::{ HttpServer, App, Responder, web };
 
 use crate::models::Status;
 
 mod models;
+mod config;
 
 async fn status() -> impl Responder {
    web::HttpResponse::Ok()
@@ -11,13 +13,20 @@ async fn status() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result <()> {
-    println! ("Starting server at http://127.0.0.1:8080");
+    dotenv().ok();
+
+    let config = crate::config::Config::from_env().unwrap();
+
+    println! (
+        "Starting server at http://{}:{}",
+        config.server.host, config.server.port
+    );
 
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(status))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(format! ("{}:{}", config.server.host, config.server.port))?
     .run()
     .await
 }
