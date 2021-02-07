abef31d4d764d0aef28582b4c96aa22f90e2b416
ermiry <erick.salas@ermiry.com>
2021-02-06 22:17:22 -0600

    Added base logger configuration

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,24 @@ use actix_web::{ HttpServer, App, web };
 
 use tokio_postgres::NoTls;
 
+use slog::{ Logger, Drain, o, info };
+use slog_term;
+use slog_async;
+
 use crate::handler::*;
 
 mod models;
 mod config;
 mod handler;
 mod db;
+mod errors;
+
+fn coonfigure_log() -> Logger {
+    let decorator = slog_term::TermDecorator::new().build();
+    let console_drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let console_drain = slog_async::Async::new(console_drain).build().fuse();
+    slog::Logger::root(console_drain, o! ("v" => env! ("CARGO_PKG_VERSION")))
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result <()> {
@@ -16,9 +28,12 @@ async fn main() -> std::io::Result <()> {
 
     let config = crate::config::Config::from_env().unwrap();
 
+    let log  = coonfigure_log();
+
     let pool = config.pg.create_pool(NoTls).unwrap();
 
-    println! (
+    info! (
+        log,
         "Starting server at http://{}:{}",
         config.server.host, config.server.port
     );
