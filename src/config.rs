f0899d3adc0ced59b521007195f629d52338d45b
ermiry <erick.salas@ermiry.com>
2021-02-07 16:12:19 -0600

    Moved log configuration to config source

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,10 @@
 use serde::Deserialize;
-use config::ConfigError;
+use slog::{ o, Drain, Logger };
+use slog_async;
+use slog_envlogger;
+use slog_term;
+
+pub use config::ConfigError;
 
 #[derive(Deserialize)]
 pub struct ServerConfig {
@@ -19,4 +24,12 @@ impl Config {
 		cfg.merge(config::Environment::new())?;
 		cfg.try_into()
 	}
+
+	pub fn configure_log() -> Logger {
+        let decorator = slog_term::TermDecorator::new().build();
+        let console_drain = slog_term::FullFormat::new(decorator).build().fuse();
+        let console_drain = slog_envlogger::new(console_drain);
+        let console_drain = slog_async::Async::new(console_drain).build().fuse();
+        slog::Logger::root(console_drain, o!("v" => env!("CARGO_PKG_VERSION")))
+    }
 }
